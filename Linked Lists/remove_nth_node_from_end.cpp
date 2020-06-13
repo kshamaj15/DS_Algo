15109f7849829b426f87f1b25ed6332592c13a12
kshama Jain <kshamaj150896@gmail.com>
2020-06-13 09:07:40 +0530

    remove nth node from end

---
+++ Linked Lists/remove_nth_node_from_end.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+class ListNode {
+    public:
+    int val;
+    ListNode *next;
+};
+
+ListNode* removeNthFromEnd(ListNode* A, int B) {
+    ListNode* fast = A;
+    ListNode* curr = A;
+    while(B--) {
+        if(fast != NULL && fast->next != NULL)
+        fast = fast->next;
+        else {
+          A = A->next;
+          break;  
+        }
+    }
+    if(A != NULL && A->next != NULL && fast!= NULL) { 
+        while(fast->next != NULL) {
+            curr = curr->next;
+            fast = fast->next;
+        }
+        curr->next = curr->next->next;
+    }
+}
+
+int main() {
+    ListNode *head = new ListNode();
+    ListNode *two = new ListNode();
+    ListNode *three = new ListNode();
+    ListNode *four = new ListNode();
+    ListNode *five = new ListNode();
+
+    head->next = NULL;
+    head->val = 5;
+    two->next = three;
+    two->val = 1;
+    three->next = four;
+    three->val = 3;
+    four->val = 6;
+    four->next = five;
+    five->val = 4;
+    five->next = NULL;
+
+    removeNthFromEnd(head, 1);
+    while (head != NULL)
+    {
+        cout<<head->val<< " ";
+        head = head->next;
+    }
+    
+
+    return 0;
+}
