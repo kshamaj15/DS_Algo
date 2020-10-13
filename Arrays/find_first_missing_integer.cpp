13d1d8eaca79f1c13fa7db357ad8c8e11774ab4a
kshamaj15 <kshamaj150896@gmail.com>
2020-10-13 09:58:14 +0530

    find missing number

--- Arrays/find_first_missing_integer.cpp
+++ Arrays/find_first_missing_integer.cpp
@@ -1,3 +1,21 @@
+// Note: numbers A[i]<=0 and A[i]>N ( N being the size of the array ) is not important to us since the missing positive integer will be in the range [1, N+1].
+// The answer will be N+1 only if all of the elements of the array are exact one occurrence of [1, N].
+// Creating buckets would have been an easy solution if using extra space was allowed.
+// An array of size N initialized to 0 would have been created.
+// For every value A[i] which lies in the range [1, N], its count in the array would have been incremented.
+
+// Finally, traversing the array would help to find the first array position with value 0 and that will be our answer.
+// However, usage of buckets is not allowed, can we use the existing array as bucket somehow?
+// Now, since additional space is not allowed either, the given array itself needs to be used to track it.
+// It may be helpful to use the fact that the size of the set we are looking to track is [1, N]
+// which happens to be the same size as the size of the array.
+// This means we can use the array to track these elements.
+// We traverse the array and if A[i] is in [1,N] range, we try to put in the index of same value in the array.
+
+
+// https://www.interviewbit.com/problems/first-missing-integer/
+// very interesting question
+
 #include<iostream>
 #include<vector>
 using namespace std;
